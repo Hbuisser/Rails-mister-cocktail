d5702d7ae9754f66ae747d95c102c8ffd797684f
Henry Buisseret <henrybuisseret@gmail.com>
2019-05-23 18:20:08 +0200

    ex1

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,32 +1,34 @@
 class DosesController < ApplicationController
-  before_action :set_params, only: [:show, :update, :edit, :destroy]
-
   def index
     @doses = Dose.all
   end
 
   def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new
+    @ingredients = Ingredient.all
   end
 
   def create
-  end
-
-  def show
-  end
-
-  def update
-  end
-
-  def edit
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
 
   def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   private
 
-  def set_params
-    @dose = Dose.find(params[:id])
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :cocktails do
-    resources :ingredients
+  resources :cocktails, only: [:index, :new, :create, :show] do
+    resources :doses, only: [:new, :create]
   end
-
-  resources :doses
+  resources :ingredients, only: [:index, :new]
+  resources :doses, only: [:destroy]
 end
